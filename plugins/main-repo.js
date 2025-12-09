9cca16701c560d8be488ef3764a3c95ec6fbe62b
rehmanabdul78600786-ctrl <rehmanabdul78600786@gmail.com>
2025-12-10 00:24:33 +0500

    Update formattedInfo bot message styling

--- plugins/main-repo.js
+++ plugins/main-repo.js
@@ -29,7 +29,7 @@ async (conn, mek, m, { from, reply }) => {
         const repoData = await response.json();
 
         // Format the repository information
-        const formattedInfo = `*BOT NAME:*\n> ${repoData.name}\n\n*OWNER NAME:*\n> ${repoData.owner.login}\n\n*STARS:*\n> ${repoData.stargazers_count}\n\n*FORKS:*\n> ${repoData.forks_count}\n\n*GITHUB LINK:*\n> ${repoData.html_url}\n\n*DESCRIPTION:*\n> ${repoData.description || 'No description'}\n\n*Don't Forget To Star and Fork Repository*\n\n> *𝐅𝐀𝐈𝐙𝐀𝐍-𝐌𝐃🖤*`;
+        const formattedInfo = `*BOT NAME:*\n> ${repoData.name}\n\n*OWNER NAME:*\n> ${repoData.owner.login}\n\n*STARS:*\n> ${repoData.stargazers_count}\n\n*FORKS:*\n> ${repoData.forks_count}\n\n*GITHUB LINK:*\n> ${repoData.html_url}\n\n*DESCRIPTION:*\n> ${repoData.description || 'No description'}\n\n*Don't Forget To Star and Fork Repository*\n\n> *꧁༒♛ 𝔅𝔒𝔖𝔖♛༒꧂💔*`;
 
         // Send image with caption
         await conn.sendMessage(from, {
